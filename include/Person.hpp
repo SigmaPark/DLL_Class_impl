df707353193f71909fdc183925a1070f705bd8cb
Sigma Park <greengb@naver.com>
2024-06-15 23:34:51 +0900

    refactoring: remove files

--- include/Person.hpp
+++ include/Person.hpp
@@ -28,6 +28,52 @@ public:
 private:
 	impl* _pimpl;
 };
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+#ifdef BUILD_DLL_LIBRARY
+	#define __PERSON_DLL_DECLSPEC __declspec(dllexport)
+#else
+	#define __PERSON_DLL_DECLSPEC __declspec(dllimport)
+#endif
+
+namespace prac
+{
+
+	extern "C"
+	{
+		__PERSON_DLL_DECLSPEC
+		Person::impl* __Create_Person_impl(char const* const name_, int const age_);
+
+		__PERSON_DLL_DECLSPEC
+		void __Destroy_Person_impl(Person::impl*& self);
+
+		__PERSON_DLL_DECLSPEC
+		char const* __Person_impl_name(Person::impl const* const self);
+
+		__PERSON_DLL_DECLSPEC
+		int __Person_impl_age(Person::impl const* const self);
+
+		__PERSON_DLL_DECLSPEC
+		void __Person_impl_Hello(Person::impl const* const person);
+	}
+
+}
+
+#undef __PERSON_DLL_DECLSPEC
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+prac::Person
+::	Person(char const* const name_, int const age_) : _pimpl( __Create_Person_impl(name_, age_) )
+{}
+
+prac::Person
+::	~Person(){  __Destroy_Person_impl(_pimpl);  }
+
+char const* prac::Person::name() const noexcept{  return __Person_impl_name(_pimpl);  }
+int prac::Person::age() const noexcept{  return __Person_impl_age(_pimpl);  }
+void prac::Person::Hello(Person const& person) noexcept{  __Person_impl_Hello(person._pimpl);  }
 
 
 #endif // end of #ifndef _PRAC_PERSON_
--- lib/Person.cpp
+++ lib/Person.cpp
@@ -1,15 +1,72 @@
 #include "Person.hpp"
-#include "Person_impl.hxx"
+#include <iostream>
 
 
-prac::Person
-::	Person(char const* const name_, int const age_) : _pimpl( __Create_Person_impl(name_, age_) )
-{}
+class prac::Person::impl
+{
+public:
+	impl(char const* const name_, int const age_);
 
-prac::Person
-::	~Person(){  __Destroy_Person_impl(_pimpl);  }
+	char const* name() const noexcept;
+	int age() const noexcept;
 
-char const* prac::Person::name() const noexcept{  return __Person_impl_name(_pimpl);  }
-int prac::Person::age() const noexcept{  return __Person_impl_age(_pimpl);  }
-void prac::Person::Hello(Person const& person) noexcept{  __Person_impl_Hello(person._pimpl);  }
+	static void Hello(impl const& person) noexcept;
+
+private:
+	char const* _name;
+	int _age;
+};
+
+
+prac::Person::impl
+::	impl(char const* const name_, int const age_) : _name(name_), _age(age_){}
+
+
+char const* prac::Person::impl
+::	name() const noexcept{  return _name;  }
+
+
+int prac::Person::impl
+::	age() const noexcept{  return _age;  }
+
+
+void prac::Person::impl::Hello(impl const& person) noexcept
+{
+	std::cout
+	<<	"Hello. My name is " << person.name() << " and I'm " << person.age() << " years old." 
+	<<	std::endl;
+}
+//========//========//========//========//=======#//========//========//========//========//=======#
+
+
+prac::Person::impl* prac::__Create_Person_impl(char const* const name_, int const age_)
+{
+	return new Person::impl(name_, age_);
+}
+
+
+void prac::__Destroy_Person_impl(Person::impl*& p)
+{
+	if(p != nullptr)
+		delete p,  
+		p = nullptr;
+}
+
+
+char const* prac::__Person_impl_name(Person::impl const* const p)
+{
+	return p->name();
+}
+
+
+int prac::__Person_impl_age(Person::impl const* const p)
+{
+	return p->age();
+}
+
+
+void prac::__Person_impl_Hello(Person::impl const* const person)
+{
+	Person::impl::Hello(*person);
+}
 //========//========//========//========//=======#//========//========//========//========//=======#
